2d878512901eaa1116cec45e852dd9fb09f6d5a7
Roelant Eijgelaar <roelanteijgelaar@hotmail.com>
2024-06-19 20:02:34 +0200

    Clean-up dependencies

---
+++ examples/dataset/ds002293-1.0.0.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds002293/CHANGES?versionId=cIP7E0YAljFfJuwUITwbn7uqUrN1n3d9 -o CHANGES
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds002293/README?versionId=XoBWLjhJ9y0bRp3HTCGrk1Lyh.SEpSWZ -o README
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds002293/dataset_description.json?versionId=z9dFemWUYdgixKO6HFs_zURnMwVWKvDI -o dataset_description.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds002293/sub-001/anat/sub-001_T1w.json?versionId=.HJM2FgGKQj2miJeN1.RU9iuFwvCdjgZ -o sub-001/anat/sub-001_T1w.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds002293/sub-001/anat/sub-001_T1w.nii.gz?versionId=2IbSfPGwDF6bwIqcgJZUaVCS.6nUn8rw -o sub-001/anat/sub-001_T1w.nii.gz
